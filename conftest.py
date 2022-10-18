c58516e45b91768a48ae6cb692970e9396d79273
Vladimir Voronin <nav-fess@yandex.ru>
2022-10-19 01:17:37 +0300

    added fixture,requirements and test file

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en", help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+
+    language = request.config.getoption("language")
+    options = webdriver.ChromeOptions()
+    options.add_argument(f"--lang={language}")
+    browser = webdriver.Chrome(options = options)
+   
+    yield browser
+
+    print("\nquit browser..")
+    browser.quit()
