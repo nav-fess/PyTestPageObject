23f2e5f192f74e3f7d10fcf663964425b9863b72
Vladimir Voronin <nav-fess@yandex.ru>
2022-10-20 15:05:10 +0300

    refactor test for login page

--- test_main_page.py
+++ test_main_page.py
@@ -23,4 +23,16 @@ def test_guest_can_go_to_login_page(browser):
     time.sleep(1)
 
 
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
+
+
+
 
