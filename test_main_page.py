49dccdf13c3fb3e2a9290649f1de4443aef7ed85
Vladimir Voronin <nav-fess@yandex.ru>
2022-10-20 12:17:07 +0300

    added assert and new test

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,22 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
 import time
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-    time.sleep(5)
+    page = MainPage(browser, link)
+    page.open()
+    time.sleep(2)
+    page.go_to_login_page()
+    time.sleep(1)
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    time.sleep(2)
+    page.should_be_login_link()
+    time.sleep(1)
